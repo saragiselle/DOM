cdcad8654f13a3496449b06216f2d0744a3bef4c
saragiselle <sarigise.miranda@gmail.com>
2023-10-30 21:14:43 -0300

    Added a new element text in blue in container1

--- js-file.js
+++ js-file.js
@@ -6,4 +6,11 @@ content.classList.add('content');
 content.textContent = "Hey I'm red";
 content.style.color = "red";
 
-container.appendChild(content);
+container.appendChild(content1);
+
+const content1 = document.createElement('h3');
+content1.classList.add('content');
+content1.textContent = "I'm a blue h3";
+content1.style.color = "blue";
+
+container.appendChild(content1);
